215c9ef666b8449ef39be0ddc95f126eb325a9ec
sluong18-occ <54555938+sluong18-occ@users.noreply.github.com>
2019-10-14 13:59:43 -0700

    Add files via upload

---
+++ InClass8A/src/CountryComparator.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+public class CountryComparator implements Comparator<Country>
+{
+    public int compare(Country a, Country b)
+    {
+        return a.getName().compareTo(b.getName());
+    }
+}
